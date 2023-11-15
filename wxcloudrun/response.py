f21a5a34c8f5505584a68889a99dd48d189ddad6
konglinghui0318 <konglh09@chinaelecom.cn>
2023-11-16 01:09:28 +0800

    添加大模型接口

--- wxcloudrun/response.py
+++ wxcloudrun/response.py
@@ -4,15 +4,19 @@ from flask import Response
 
 
 def make_succ_empty_response():
-    data = json.dumps({'code': 0, 'data': {}})
+    data = json.dumps({'err_code': 0, 'data_list': []})
     return Response(data, mimetype='application/json')
 
 
 def make_succ_response(data):
-    data = json.dumps({'code': 0, 'data': data})
+    data = json.dumps({'err_code': 0, 'data_list': [data]})
     return Response(data, mimetype='application/json')
 
 
 def make_err_response(err_msg):
-    data = json.dumps({'code': -1, 'errorMsg': err_msg})
+    data = json.dumps({'err_code': -1, 'data_list': [err_msg]})
     return Response(data, mimetype='application/json')
+
+
+if __name__ == '__main__':
+    print(make_succ_response({"param_a": "content_a"}).response)
--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -4,6 +4,8 @@ from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+import requests
+import json
 
 
 @app.route('/')
@@ -14,53 +16,28 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/api/count', methods=['POST'])
-def count():
-    """
-    :return:计数结果/清除结果
-    """
-
-    # 获取请求体参数
+@app.route('/api/llm', methods=['POST'])
+def llm():
     params = request.get_json()
 
     # 检查action参数
-    if 'action' not in params:
-        return make_err_response('缺少action参数')
+    if 'content' not in params:
+        return make_err_response('缺少content参数')
 
-    # 按照不同的action的值，进行不同的操作
-    action = params['action']
+    content = params['content']
 
-    # 执行自增操作
-    if action == 'inc':
-        counter = query_counterbyid(1)
-        if counter is None:
-            counter = Counters()
-            counter.id = 1
-            counter.count = 1
-            counter.created_at = datetime.now()
-            counter.updated_at = datetime.now()
-            insert_counter(counter)
-        else:
-            counter.id = 1
-            counter.count += 1
-            counter.updated_at = datetime.now()
-            update_counterbyid(counter)
-        return make_succ_response(counter.count)
-
-    # 执行清0操作
-    elif action == 'clear':
-        delete_counterbyid(1)
-        return make_succ_empty_response()
-
-    # action参数错误
-    else:
-        return make_err_response('action参数错误')
+    return get_llm(content)
 
 
-@app.route('/api/count', methods=['GET'])
-def get_count():
-    """
-    :return: 计数的值
-    """
-    counter = Counters.query.filter(Counters.id == 1).first()
-    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+def get_llm(content):
+    url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/eb-instant?access_token=24.7a649d915fe0b59c319416b9d55b65e8.2592000.1702655299.282335-42777740"
+
+    payload = json.dumps({"messages": [{"role": "user", "content": content}]})
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.json()['result']
+
